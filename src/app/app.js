bb3148fb8748d53b52b21ecb4a643b4cfc288615
André Souza <andresouza@meraki.dev.br>
2022-08-18 22:34:36 -0300

    [ADD] Authenticate

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require("morgan");
+const routes = require("./routes/routes");
+const mongoose = require("mongoose")
+const cors = require("cors")
+
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+  useUnifedTopology: true,
+  useNewUrlParse: true
+});
 
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 app.get("/", (request,response) => {
   return response.status(200).json({
@@ -18,5 +28,18 @@ app.get("/", (request,response) => {
     }
   })
 });
+
+app.use(routes);
+app.use((error, request, response, next ) => {
+  if(error.joi){
+    return response
+      .status(400)
+      .json({
+        error: error.joi.message
+      });
+  }
+
+  return response.status(500).send(error);
+})
 
 module.exports = app;
